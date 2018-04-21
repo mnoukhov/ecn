8fe17bdb4fbaa07ae526e719f7ce9c75084ba880
Michael Noukhovitch <michael.noukhovitch@umontreal.ca>
2018-04-21 00:56:40 -0400

    selfish opponent util script

---
+++ scripts/self-opputil.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ~/.bashrc
+source activate emerge
+export PROJECTROOT="~/iter-comm"
+export PYTHONPATH=$PROJECTROOT:$PYTHONPATH
+
+name="self-opputil$1"
+
+python src/ecn.py \
+    --enable-cuda \
+    --model-file "model_saves/$name.dat" \
+    --name $name \
+    --disable-comms \
+    --enable-opponent-utility $1 \
+    --term-entropy-reg 0.05 \
+    --utterance-entropy-reg 0.0001 \
+    --proposal-entropy-reg 0.005 \
+    --render-every-seconds 120 \
+    --save-every-seconds 360 \
+    --episodes 500000 \
