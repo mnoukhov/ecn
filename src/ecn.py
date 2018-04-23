4ae120a1f6e4b1024b87d3e8fe1b92d0e7c97a87
Michael Noukhovitch <mnoukhov@gmail.com>
2018-04-23 04:36:38 -0400

    explicit argparse options

--- src/ecn.py
+++ src/ecn.py
@@ -101,7 +101,7 @@ def run_episode(
     enable_cuda,
     enable_comms,
     enable_proposal,
-    opponent_utility_comms,
+    comms_opponent_utility,
     prosocial,
     agent_models,
     # batch_size,
@@ -143,11 +143,11 @@ def run_episode(
 
         agent_model = agent_models[agent]
         if enable_comms:
-            if opponent_utility_comms is None:
+            if comms_opponent_utility is None:
                 _prev_message = s.m_prev
-            elif ((opponent_utility_comms == 0 and agent == 0) or
-                  (opponent_utility_comms == 1 and agent == 1) or
-                  (opponent_utility_comms == 2)):
+            elif ((comms_opponent_utility == 0 and agent == 0) or
+                  (comms_opponent_utility == 1 and agent == 1) or
+                  (comms_opponent_utility == 2)):
                 # 0: agent 0 sees agent 1 utility
                 # 1: agent 1 sees agent 0 utility
                 # 2: both agents see each other's utility
@@ -300,7 +300,7 @@ def run(args):
              enable_cuda=args.enable_cuda,
              enable_comms=args.enable_comms,
              enable_proposal=args.enable_proposal,
-             opponent_utility_comms=args.opponent_utility_comms,
+             comms_opponent_utility=args.comms_opponent_utility,
              agent_models=agent_models,
              prosocial=args.prosocial,
              # batch_size=batch_size,
@@ -360,7 +360,7 @@ def run(args):
                      enable_cuda=args.enable_cuda,
                      enable_comms=args.enable_comms,
                      enable_proposal=args.enable_proposal,
-                     opponent_utility_comms=args.opponent_utility_comms,
+                     comms_opponent_utility=args.comms_opponent_utility,
                      agent_models=agent_models,
                      prosocial=args.prosocial,
                      render=True,
@@ -439,46 +439,53 @@ def run(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # game args
+    # game fixed args
     game_args = parser.add_argument_group('game args')
     game_args.add_argument('--utterance-max-length', type=int, default=6)
     game_args.add_argument('--utterance-vocab-size', type=int, default=11)
     game_args.add_argument('--item-max-quantity', type=int, default=6)
     game_args.add_argument('--item-max-utility', type=int, default=11)
     game_args.add_argument('--num-items', type=int, default=11)
+
     # model info
     parser.add_argument('--model-file', type=str, default='model_saves/model.dat')
     parser.add_argument('--batch-size', type=int, default=128)
     parser.add_argument('--seed', type=int, help='optional')
     parser.add_argument('--test-seed', type=int, default=123, help='used for generating test game set')
     parser.add_argument('--episodes', type=int, default=5e5, help='total number of episodes to run')
+    parser.add_argument('--enable-cuda', action='store_true')
+
     # hyperparams
     parser.add_argument('--term-entropy-reg', type=float, default=0.05)
     parser.add_argument('--utterance-entropy-reg', type=float, default=0.001)
     parser.add_argument('--proposal-entropy-reg', type=float, default=0.05)
-    #game
-    parser.add_argument('--disable-proposal', action='store_true')
-    parser.add_argument('--disable-comms', action='store_true')
-    parser.add_argument('--disable-prosocial', action='store_true')
+
+    # agents
+    social = parser.add_mutually_exclusive_group(required=True)
+    social.add_argument('--prosocial', dest='prosocial', action='store_true')
+    social.add_argument('--selfish', dest='prosocial', action='store_false')
+    proposal = parser.add_mutually_exclusive_group(required=True)
+    proposal.add_argument('--enable-proposal', dest='enable_proposal', action='store_true')
+    proposal.add_argument('--disable-proposal', dest='enable_proposal', action='store_false')
+    comms = parser.add_mutually_exclusive_group(required=True)
+    comms.add_argument('--enable-comms', dest='enable_comms', action='store_true')
+    comms.add_argument('--disable-comms', dest='enable_comms', action='store_false')
+
+    # loading / logging
     parser.add_argument('--render-every-seconds', type=int, default=30)
     parser.add_argument('--save-every-seconds', type=int, default=30)
     parser.add_argument('--testing', action='store_true', help='turn off learning; always pick argmax')
-    parser.add_argument('--enable-cuda', action='store_true')
     parser.add_argument('--no-load', action='store_true')
     parser.add_argument('--no-save', action='store_true')
     parser.add_argument('--name', type=str, default='', help='used for logfile naming')
     parser.add_argument('--logfile', type=str, default='logs/log_%Y%m%d_%H%M%S{name}.log')
+
     # experiments
-    parser.add_argument('--opponent-utility-comms', type=int, default=None)
+    parser.add_argument('--comms-opponent-utility', type=int, default=None)
     parser.add_argument('--utility-normalization', action='store_true')
+
     args = parser.parse_args()
-    args.enable_comms = not args.disable_comms
-    args.enable_proposal = not args.disable_proposal
-    args.prosocial = not args.disable_prosocial
     args.logfile = args.logfile.format(**args.__dict__)
     args.logfile = datetime.datetime.strftime(datetime.datetime.now(), args.logfile)
-    del args.__dict__['disable_comms']
-    del args.__dict__['disable_proposal']
-    del args.__dict__['disable_prosocial']
     del args.__dict__['name']
     run(args)
