031756cf360ab878952990ffe7fc6b7e5d0b644d
Michael Noukhovitch <noukhovm@cedar5.cedar.computecanada.ca>
2018-04-22 23:05:27 -0700

    selfish proposal channel script

---
+++ scripts/self-prop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mem=4G
+#SBATCH --time=0-10:00
+
+source ~/.bashrc
+source activate emerge
+export PROJECTROOT="~/iter-comm"
+export PYTHONPATH=$PROJECTROOT:$PYTHONPATH
+
+name="self-prop"
+
+python src/ecn.py \
+    --enable-cuda \
+    --model-file "model_saves/$name.dat" \
+    --name $name \
+    --disable-comms \
+    --term-entropy-reg 0.05 \
+    --utterance-entropy-reg 0.0001 \
+    --proposal-entropy-reg 0.005 \
+    --render-every-seconds 120 \
+    --save-every-seconds 360 \
+    --episodes 500000 \
