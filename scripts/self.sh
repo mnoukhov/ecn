26b1cc70e92bfd5507ab70bdf366005c9465875c
Michael Noukhovitch <michael.noukhovitch@umontreal.ca>
2018-10-26 02:48:04 -0400

    self script

---
+++ scripts/self.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python src/main.py \
+    --noprosocial \
+    --enable_cuda \
+    --model_dir "model_saves" \
+    --logdir "logs" \
+    --term-entropy-reg 0.05 \
+    --utterance-entropy-reg 0.0001 \
+    --proposal-entropy-reg 0.005 \
+    --render-every-episode 200 \
+    --save-every-seconds 360 \
+    --episodes 500000 \
