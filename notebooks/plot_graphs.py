5c2b78f4ed816a21a7599dfa4ddca51d47e2230c
Michael Noukhovitch <mnoukhov@gmail.com>
2018-10-26 21:53:52 -0400

    better plotting

--- notebooks/plot_graphs.py
+++ notebooks/plot_graphs.py
@@ -5,79 +5,33 @@ import matplotlib.pyplot as plt
 import json
 import argparse
 import numpy as np
+import glob
 
 
-def plot_reward(logfile, title=None, min_y=None, max_y=None, max_x=None, 
-                show_train=False, show_both=True):
-    """
-    logfiles separated by : are combined
-    logfiles separated by , go in separate plots
-    (: binds tighter than ,)
-    """
-    logfiles = logfile
-    split_logfiles = logfiles.split(',')
-    for j, logfile_groups in enumerate(split_logfiles):
-        epoch = []
-        reward_both = []
-        reward_A = []
-        reward_B = []
-        test_reward_both = []
-        test_reward_A = []
-        test_reward_B = []
-        reward_names = {
-            'train reward both': reward_both,
-            'train reward A': reward_A,
-            'train reward B': reward_B,
-            'test reward both': test_reward_both,
-            'test reward A': test_reward_A,
-            'test reward B': test_reward_B,
-        }
+def plot_one(logfile, title=None, min_y=None, max_y=None,
+             show_train=False, show_both=True):
+    rewards = parse_logfile(logfile)
+    epochs = rewards.pop('epochs')
+    epochs = np.array(epochs) / 1000
 
-        for logfile in logfile_groups.split(':'):
-            with open(logfile, 'r') as f:
-                for n, line in enumerate(f):
-                    if n == 0:
-                        print(logfile, line)
-                        continue  # skip first line
-                    line = line.strip()
-                    if line == '':
-                        continue
-                    d = json.loads(line)
-                    if max_x is not None and d['episode'] > max_x:
-                        continue
-                    epoch.append(int(d['episode']))
-                    reward_both.append(float(d['avg_reward_0']))
-                    if 'avg_reward_A' in d:
-                        reward_A.append(float(d['avg_reward_A']))
-                    if 'avg_reward_B' in d:
-                        reward_B.append(float(d['avg_reward_B']))
-                    if 'test_reward' in d:
-                        test_reward_both.append(d['test_reward'])
-                    if 'test_reward_A' in d:
-                        test_reward_A.append(float(d['test_reward_A']))
-                    if 'test_reward_B' in d:
-                        test_reward_B.append(float(d['test_reward_B']))
-        print('epoch[0]', epoch[0], 'epochs[-1]', epoch[-1])
-        if min_y is None:
-            min_y = 0
-        if max_y is not None:
-            plt.ylim([min_y, max_y])
-        suffix = ''
-        if len(split_logfiles) > 0:
-            suffix = ' %s' % (j + 1)
+    if min_y is None:
+        min_y = 0
+    if max_y is not None:
+        plt.ylim([min_y, max_y])
 
-        for name, reward in reward_names.items():
-            if not reward:
-                continue
-            elif not show_train and ('train' in name):
-                continue
-            elif not show_both and ('both' in name):
-                continue
-            else:
-                plt.plot(np.array(epoch) / 1000, reward, label=name + suffix)
+    for name, values in rewards.items():
+        if not values:
+            continue
+        elif not show_train and ('train' in name):
+            continue
+        elif not show_both and ('both' in name):
+            continue
+        else:
+            plt.plot(epochs, values, label=name)
 
-    if title is not None:
+    if title:
         plt.title(title)
+
     plt.xlabel('Episodes of 128 games (thousands)')
     plt.ylabel('Normalized Reward')
     plt.legend()
@@ -85,15 +39,93 @@ def plot_reward(logfile, title=None, min_y=None, max_y=None, max_x=None,
     # plt.savefig('/tmp/out-reward.png')
 
 
+def plot_average(logdir, title=None, min_y=None, max_y=None,
+                 show_train=False, show_both=True):
+    # if not os.path.isabs(folder):
+        # folder = os.path.join(logdir, folder)
+    # if not os.isdir(folder):
+        # raise Exception('not a folder')
+    logfiles = glob.glob('{}/*.log'.format(logdir))
+
+    parsed = [parse_logfile(logfile) for logfile in logfiles]
+
+    # cut off at minimum of all logs
+    epochs = [log.pop('epochs') for log in parsed]
+    length = min(len(epoch) for epoch in epochs)
+    epochs = np.array(epochs[0][:length]) / 1000
+
+    for name in parsed[0].keys():
+        if not show_train and ('train' in name):
+            continue
+        elif not show_both and ('both' in name):
+            continue
+        else:
+            values_arr = np.array([log[name][:length] for log in parsed])
+            avg_values = values_arr.mean(axis=0)
+            plt.plot(epochs, avg_values, label=name)
+
+    if title:
+        plt.title(title)
+
+    if min_y is None:
+        min_y = 0
+    if max_y is not None:
+        plt.ylim([min_y, max_y])
+
+    plt.xlabel('Episodes of 128 games (thousands)')
+    plt.ylabel('Normalized Reward')
+    plt.legend()
+    plt.show()
+    # plt.savefig('/tmp/out-reward.png')
+
+
+
+def parse_logfile(logfile):
+    rewards = {
+        'epochs': [],
+        'train reward both': [],
+        'train reward A': [],
+        'train reward B': [],
+        'test reward both': [],
+        'test reward A': [],
+        'test reward B': [],
+    }
+    json_to_name = {
+        'episode': 'epochs',
+        'avg_reward_0': 'train reward both',
+        'avg_reward_A': 'train reward A',
+        'avg_reward_B': 'train reward B',
+        'test_reward': 'test reward both',
+        'test_reward_A': 'test reward A',
+        'test_reward_B': 'test reward B',
+    }
+
+    with open(logfile, 'r') as f:
+        for n, line in enumerate(f):
+            if n == 0:
+                print(logfile, line)
+                continue  # skip first line
+            line = line.strip()
+            if line == '':
+                continue
+
+            d = json.loads(line)
+            for item, value in d.items():
+                name = json_to_name.get(item)
+                if name:
+                    rewards[name].append(float(value))
+
+    return rewards
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('logfile', type=str)
+    parser.add_argument('logdir', type=str)
     parser.add_argument('--title', type=str)
-    parser.add_argument('--max-x', type=int)
     parser.add_argument('--min-y', type=float)
     parser.add_argument('--max-y', type=float)
-    parser.add_argument('--show_test', action='store_true')
+    parser.add_argument('--show_train', action='store_true')
     parser.add_argument('--show_both', action='store_true')
 
     args = parser.parse_args()
-    plot_reward(**vars(args))
+    plot_average(**vars(args))
