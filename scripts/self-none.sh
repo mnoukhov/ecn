05e77d90b5c753440e0a9fe252b148008d9a9119
Michael <michael.noukhovitch@elementai.com>
2020-01-23 13:38:22 -0500

    remove enable-cude, env bash

--- scripts/self-none.sh
+++ scripts/self-none.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 python src/main.py \
     --name "self-none" \
     --noprosocial \
     --nolinguistic \
     --noproposal \
-    --enable_cuda \
     --term-entropy-reg 0.2 \
     --proposal-entropy-reg 0.005 \
     --episodes 250000 \
