772af8bc4e11fdacfa0361965cd6bc6e7ef536bd
Michael Noukhovitch <michael.noukhovitch@elementai.com>
2020-02-05 04:33:31 +0000

    borgy script

--- scripts/borgy.sh
+++ scripts/borgy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+HASH_ID=$(docker inspect --format="{{.Id}}" volatile-images.borgy.elementai.net/mnoukhov/ecn:latest)
+DOCKER_ID=${HASH_ID:7}
+
 borgy submit \
-    --gpu 1 \
-    -v /home/mnoukhov/
+    --gpu 0 \
+    --mem 4 \
+    -e WANDB_API_KEY=$WANDB_API_KEY \
+    -e WANDB_DOCKER=$DOCKER_ID \
+    -e HOME=/workdir \
+    -i volatile-images.borgy.elementai.net/mnoukhov/ecn:latest \
+    -v /home/mnoukhov/ecn:/workdir/ecn:ro \
+    -- bash -c "cd ecn; source ./scripts/local.sh; $@"
