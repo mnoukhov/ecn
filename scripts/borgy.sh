e04c73b798ce96026a054e021997309c811f801c
Michael Noukhovitch <michael.noukhovitch@elementai.com>
2020-02-07 16:19:16 +0000

    borgy over seeds

--- scripts/borgy.sh
+++ scripts/borgy.sh
@@ -3,12 +3,17 @@
 HASH_ID=$(docker inspect --format="{{.Id}}" volatile-images.borgy.elementai.net/mnoukhov/ecn:latest)
 DOCKER_ID=${HASH_ID:7}
 
-borgy submit \
-    --gpu 0 \
-    --mem 4 \
-    -e WANDB_API_KEY=$WANDB_API_KEY \
-    -e WANDB_DOCKER=$DOCKER_ID \
-    -e HOME=/workdir \
-    -i volatile-images.borgy.elementai.net/mnoukhov/ecn:latest \
-    -v /home/mnoukhov/ecn:/workdir/ecn:ro \
-    -- bash -c "cd ecn; source ./scripts/local.sh; $@"
+for SEED in 0 1 2 3 4
+do
+    borgy submit \
+        --gpu 0 \
+        --mem 4 \
+        -e WANDB_API_KEY=$WANDB_API_KEY \
+        -e WANDB_DOCKER=$DOCKER_ID \
+        -e SEED=$SEED \
+        -e HOME=/workdir \
+        -i volatile-images.borgy.elementai.net/mnoukhov/ecn:latest \
+        -v /home/mnoukhov/ecn:/workdir/ecn:ro \
+        -- bash -c "cd ecn; source ./scripts/local.sh; $@"
+    sleep 2
+done
